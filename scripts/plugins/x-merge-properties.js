2efce646ef82105a8c2139cd98b8eda57f6dfc58
k.struzhkin <k.struzhkin@rbkadmins-MacBook-Pro.local>
2019-06-18 11:25:16 +0300

    Fix plugin

--- scripts/plugins/x-merge-properties.js
+++ scripts/plugins/x-merge-properties.js
@@ -2,25 +2,28 @@ var jpointer = require('json-pointer');
 var mergePatch = require('json-merge-patch');
 
 module.exports = {
-  pathExpression: '$..["x-rebillyMerge"]',
+  pathExpression: '$..["x-merge-properties"]',
   init: function(swagger) {
-    console.log('* x-rebillyMerge plugin');
+    console.log('* x-merge-properties plugin');
   },
   process: function(parent, name, jsonpath, swagger) {
     var value = parent[name];
     if (!Array.isArray(value)) {
-      throw Error('x-rebillyMerge argument should be array at ' + jsonpath);
+      throw Error('x-merge-properties argument should be array at ' + jsonpath);
     }
-    let res = null;
+    let required = [];
+    let properties = {};
     value.forEach(function(obj) {
-      if (typeof obj !== 'object') throw Error('Can\'t merge non-object values at ' + jsonpath);
       if (obj.$ref && (typeof obj.$ref === 'string')) {
         obj = jpointer.get(swagger, obj.$ref.substring(1));
       }
-      res = mergePatch.apply(res, obj);
+      if (typeof obj !== 'object') throw Error('Can\'t merge non-object values at ' + jsonpath);
+      required = required.concat(obj.required || []);
+      properties = mergePatch.apply(properties, obj.properties || {});
     });
     delete parent[name];
-    Object.assign(parent, res);
+    parent.required = required;
+    parent.properties = properties;
   },
   finish: function(swagger) {
     // TODO: cleanup unused $refs
